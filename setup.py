a8b933832785a5876406cd4b718b38cfdeed0d97
Jiseon Kim <ji.dozin@gmail.com>
2025-06-22 00:57:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup
 
+package_name = 'respeaker_ros2'
+
 setup(
-    name='respeaker_ros2',
+    name=package_name,
     version='0.0.1',
-    packages=['respeaker_ros2'],
-    install_requires=[
-        'rclpy',
-        'pyusb',
-        'pyaudio',
-        'numpy',
-        'click',
-    ],
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Jiseon Ki',
+    author_email='jiseon.ki@example.com',
+    maintainer='Jiseon Ki',
+    maintainer_email='jiseon.ki@example.com',
+    description='ROS 2 package for ReSpeaker USB Microphone Array v2.0 to publish audio data and DOA.',
+    license='Apache 2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'audio_with_doa_node = respeaker_ros2.audio_with_doa_publisher:main',
+            'audio_with_doa_publisher = respeaker_ros2.audio_with_doa_publisher:main',
         ],
     },
 )
